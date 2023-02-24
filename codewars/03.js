9b575c1659b080dcdcf854d47abf69290a12554e
sergfila <sergfila@live.ru>
2023-02-24 10:40:21 +0300

    03 cw

---
+++ codewars/03.js
@@ -0,0 +1,16 @@
+function alphabetPosition(text) {
+
+    let result = "";
+    text = text.toLowerCase();
+
+    for (let item of text) {
+        let table = item.charCodeAt() - 96;
+        if (table > 0 && table < 26)
+            result += table + ' ';
+    }
+
+    return result.slice(0, -1); ;
+}
+
+let str = "The sunset sets at twelve o' clock."
+console.log(alphabetPosition(str));
