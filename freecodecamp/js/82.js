9b785ad278bac66cc245bd19f76286f18f0bf05e
sergfila <sergfila@live.ru>
2023-03-03 22:53:59 +0300

    82 js

---
+++ freecodecamp/js/82.js
@@ -0,0 +1,10 @@
+const myDog = {
+    // Only change code below this line
+
+    "name": "Shkila",
+    "legs": 4,
+    "tails": 0,
+    "friends": ["sergfila", "nesterzhzot"]
+
+    // Only change code above this line
+};
